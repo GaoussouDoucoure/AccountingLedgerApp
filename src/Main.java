3caee615664fed4d4fee3dcf194db58dd802e352
Gaoussou Doucoure <gaousd4@gmail.com>
2025-04-29 15:43:38 -0700

    Updated the loops and conditionals of the Main class to better the outputs for the users

--- src/Main.java
+++ src/Main.java
@@ -1,131 +1,111 @@
-import java.io.File;
-import java.io.FileNotFoundException;
-import java.util.ArrayList;
 import java.util.Scanner;
 
 public class Main {
     public static void main(String[] args) {
-        ArrayList<Transaction> transaction = new ArrayList<>();
-        File file = new File("transactions.csv");
-        try {
-            Scanner reader = new Scanner(file);
-            if(reader.hasNextLine()){
-                String header = reader.nextLine();
-//                System.out.println(header); //To cut off the 1st line of the file since they are Titles and all strings which will cause problem on conversion for "amount"
-            }
-            while(reader.hasNextLine()){
-                String line = reader.nextLine();
-//                System.out.println(line);
-            }
-        } catch (FileNotFoundException e) {
-            throw new RuntimeException(e);
-        }
-        Screens.homeScreen();
         Scanner sc = new Scanner(System.in);
         String userInput = "";
-        while (!userInput.equalsIgnoreCase("X")) { // start of WHILE loop for Home Screen
-            userInput = sc.nextLine();
-            if (userInput.equalsIgnoreCase("X")) {   // start of Conditionals Home Screen
-                System.out.println("You have exited out of the application. See you next time :)");
-                return;
+
+        // Outer loop for the Home Screen
+        while (!userInput.equalsIgnoreCase("X")) {
+            Screens.homeScreen();  // Display Home Screen
+            System.out.print("Enter an option: ");
+            userInput = sc.nextLine().trim();
+
+            if (userInput.equalsIgnoreCase("X")) {
+                System.out.println("You have exited the application. See you next time!");
+                break;
             } else if (userInput.equalsIgnoreCase("D")) {
-                System.out.println("Enter a deposit (e.g. 2023-04-15|11:15:00|Invoice 1001 paid|Joe|1500.00:");
-                String deposit = sc.nextLine();
-                // TODO say deposit successful, you're back to the previous screen
-                System.out.println("Your deposit is made. Now you are back to the previous Screen.\n");
-                Screens.homeScreen();
+                // Process deposit here
+                System.out.println("Enter a deposit (e.g. 2023-04-15|11:15:00|Invoice 1001 paid|Joe|1500.00): ");
+                String depositInput = sc.nextLine();
+                // TODO: Process deposit and add to ledger.
+                System.out.println("Your deposit was successful. Returning to Home Screen.\n");
+                // After the action, the loop restarts and re-displays the home screen.
             } else if (userInput.equalsIgnoreCase("P")) {
-                System.out.println("Enter a payment (e.g. 2023-04-15|10:13:25|ergonomic keyboard|Amazon|-89.50:");
-                String deposit = sc.nextLine();
-                // TODO say payment successful, you're back to the previous screen
-                System.out.println("Your payment is made. Now you are back to the previous Screen.\n");
-                Screens.homeScreen();
+                // Process payment here
+                System.out.println("Enter a payment (e.g. 2023-04-15|10:13:25|ergonomic keyboard|Amazon|-89.50): ");
+                String paymentInput = sc.nextLine();
+                // TODO: Process payment and add to ledger.
+                System.out.println("Your payment was successful. Returning to Home Screen.\n");
             } else if (userInput.equalsIgnoreCase("L")) {
-                Screens.ledgerScreen();
-                userInput = "";
-                while (!userInput.equalsIgnoreCase("H")) {   // start of WHILE loop for Ledger Screen
-                    userInput = sc.nextLine();
-                    if (userInput.equalsIgnoreCase("H")) {   // start of Conditionals Ledger Screen
-                        Screens.homeScreen();
-                        break;
-                    } else if (userInput.equalsIgnoreCase("A")) {
-                        System.out.println("Here is all the entries");
-                        //TODO display all the entries
-                        // Show Ledger Screen since they will go back to it with "break" below
-                        break;
-                    } else if (userInput.equalsIgnoreCase("D")) {
-                        System.out.println("Here is all your deposits:");
-                        //TODO display all the deposits
-                        // Show Ledger Screen since they will go back to it with "break" below
-                        break;
-                    } else if (userInput.equalsIgnoreCase("P")) {
-                        System.out.println("Here is all your payments:");
-                        //TODO display all the payments
-                        // Show Ledger Screen since they will go back to it with "break" below
-                        break;
-                    } else if (userInput.equalsIgnoreCase("R")) {
-                        Screens.reportScreen();
-                        int command = -1;
-                        while (command != 6) {   // start of WHILE loop for Report Screen
-                            command = sc.nextInt();
-                            sc.nextLine();
-                            if (command == 1) {     // start of Conditionals for Report Screen
-                                System.out.println("Month To Date:");
-                                //TODO display reports from Month To Date
-                                // Show Report Screen since they will go back to it with "break" below
-                                break;
-                            } else if (command == 2) {
-                                System.out.println("Previous Month");
-                                //TODO display reports of Previous Month
-                                // Show Report Screen since they will go back to it with "break" below
-                                break;
-                            } else if (command == 3) {
-                                System.out.println("Year To Date");
-                                //TODO display reports of Year To Date
-                                // Show Report Screen since they will go back to it with "break" below
-                                break;
-                            } else if (command == 4) {
-                                System.out.println("Previous Year");
-                                //TODO display reports of Previous Year
-                                // Show Report Screen since they will go back to it with "break" below
-                                break;
-                            } else if (command == 5) {
-                                System.out.println("search by Vendor");
-                                //TODO display all entries for that vendor
-                                // Show Report Screen since they will go back to it with "break" below
-                                break;
-                            } else if (command == 6) {
-                                Screens.ledgerScreen();
-                                break;
-                            } else if (command == 0) {
-                                System.out.println("You have exited out of the application. See you next time :)");
-                                return;
+                // Enter Ledger Screen: start inner loop
+                String ledgerInput = "";
+                while (!ledgerInput.equalsIgnoreCase("H")) {
+                    Screens.ledgerScreen();  // Display the Ledger Screen
+                    System.out.print("Enter a ledger option: ");
+                    ledgerInput = sc.nextLine().trim();
+
+                    if (ledgerInput.equalsIgnoreCase("H")) {
+                        // User requests to go Home
+                        System.out.println("Returning to Home Screen...\n");
+                        break;  // Breaks out of the ledger loop back to the Home Screen.
+                    } else if (ledgerInput.equalsIgnoreCase("A")) {
+                        System.out.println("Displaying all entries...");
+                        // TODO: Display all ledger entries.
+                        // Re-display the ledger screen after completing the task.
+                        continue;
+                    } else if (ledgerInput.equalsIgnoreCase("D")) {
+                        System.out.println("Displaying all deposits...");
+                        // TODO: Display deposit-only entries.
+                        continue;
+                    } else if (ledgerInput.equalsIgnoreCase("P")) {
+                        System.out.println("Displaying all payments...");
+                        // TODO: Display payment-only entries.
+                        continue;
+                    } else if (ledgerInput.equalsIgnoreCase("R")) {
+                        // Enter Report Screen: another inner loop
+                        String reportInput = "";
+                        while (!reportInput.equals("6")) {
+                            Screens.reportScreen();  // Display report menu
+                            System.out.print("Enter a report option: ");
+                            reportInput = sc.nextLine().trim();
+
+                            if (reportInput.equals("1")) {
+                                System.out.println("Month To Date Report...");
+                                // TODO: Display Month To Date report.
+                                continue;
+                            } else if (reportInput.equals("2")) {
+                                System.out.println("Previous Month Report...");
+                                // TODO: Display Previous Month report.
+                                continue;
+                            } else if (reportInput.equals("3")) {
+                                System.out.println("Year To Date Report...");
+                                // TODO: Display Year To Date report.
+                                continue;
+                            } else if (reportInput.equals("4")) {
+                                System.out.println("Previous Year Report...");
+                                // TODO: Display Previous Year report.
+                                continue;
+                            } else if (reportInput.equals("5")) {
+                                System.out.println("Search by Vendor...");
+                                // TODO: Prompt user for vendor name and display relevant entries.
+                                continue;
+                            } else if (reportInput.equals("6")) {
+                                System.out.println("Returning to Ledger Screen...\n");
+                                break;  // Exit the report loop back to the ledger screen.
+                            } else if (reportInput.equals("0")) {
+                                System.out.println("Exiting application. Goodbye!");
+                                return;  // Exit the entire application.
                             } else {
-                                System.out.printf("""
-                                        You entered an invalid command: %s
-                                        Please try again!
-                                        """, command);
-                                ;
-                            } // end of Report Screen Conditionals
-                        } // end of WHILE loop for Report Screen
-
-                    } else if (userInput.equalsIgnoreCase("X")) {
-                        System.out.println("You have exited out of the application. See you next time :)");
-                        return;
+                                System.out.printf("You entered an invalid report option: %s. Please try again!\n", reportInput);
+                            }
+                        }
+                        // After exiting the report loop, continue with the ledger loop.
+                        continue;
+                    } else if (ledgerInput.equalsIgnoreCase("X")) {
+                        System.out.println("Exiting application. Goodbye!");
+                        return;   // Exit the entire application.
                     } else {
-                        System.out.printf("""
-                                You entered an invalid command: %s
-                                Please try again!
-                                """, userInput);
-                    } // end of Ledger Screen Conditionals
-                } // end of WHILE loop for Ledger Screen
+                        System.out.printf("You entered an invalid ledger option: %s. Please try again!\n", ledgerInput);
+                    }
+                }  // End of ledger inner loop.
+                // After breaking out of the ledger loop, the outer home loop will re-display the Home Screen.
             } else {
-                System.out.printf("""
-                        You entered an invalid command: %s
-                        Please try again!
-                        """, userInput);
-            } // end of Home Screen Conditionals
-        } // end of WHILE loop for Home Screen
+                System.out.printf("You entered an invalid option: %s. Please try again!\n", userInput);
+            }
+        }  // End of Home Screen loop.
 
+        System.out.println("Application has terminated.");
+        sc.close();
     }
 }
